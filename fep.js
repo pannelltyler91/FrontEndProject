69ca539cf3ca5338af7d79144a0a00c56c637125
Tyler Pannell <pannelltyler91@gmail.com>
2021-01-06 22:37:07 -0500

    working functionality of search bar & added text above search bar - Tyler

--- fep.js
+++ fep.js
@@ -1,1 +1,35 @@
 //this is a javascript file
+
+var searchInput = $('#searchBar').val();
+
+
+$('#searchButton').click(function(e){
+    e.preventDefault();
+   $.ajax({
+        type:'GET',
+        url:'https://api.jikan.moe/v3/search/anime?q=Haikyu',
+        success:function(data){
+            console.log('success',data.results[0]);
+        }
+
+   });
+
+});
+
+
+
+$('#goToReviews').click(function(e){
+
+    $.ajax({
+        type:'GET',
+        url:'https://api.jikan.moe/v3/anime/20583/reviews',
+        success:function(data){
+            console.log('success',data.reviews[0]);
+        }
+    
+    });
+
+});
+
+   
+
