a73e9a8df16b73b928e69ced40ddadfc700a721e
Tyler Pannell <pannelltyler91@gmail.com>
2021-01-14 18:19:11 -0500

    search bar completed and CSS for cards even,only missing review button functionality-Tyler

--- fep.js
+++ fep.js
@@ -1,62 +1,62 @@
 //this is a javascript file
 
-
-// renders anime pulled from Jikan api into a specific card
-function renderAnime (){
-    var searchInput = $('#searchBar').val();
-   $.ajax({
-        type:'GET',
-        url:'https://api.jikan.moe/v3/search/anime?q='+searchInput,
-        success:function(data){
-            var superSaiyan = data.results;
-            console.log('success',superSaiyan);
-            console.log(searchInput);
-            let output = '';
-            superSaiyan.each(function (anime){
-                output = 
-                `
-                
-                
-                
-                
-                
-                
-                `
-            })
-            
-        }
-        
-   });
-
-}
-
-//------------------------------------------------------------------------------------------------------------------
-
-
-
-
 $('#searchButton').click(function(e){
     e.preventDefault();
     var searchInput = $('#searchBar').val();
-   renderAnime();
+     $.ajax({
+         type:'GET',
+         url:'https://api.jikan.moe/v3/search/anime?q='+searchInput,
+         success:function(data){
+             var superSaiyan = data.results.slice(0,9);
+             console.log(superSaiyan);
+             let output = '';
+             $.each(superSaiyan,function(index,value){
+               console.log(value);
+                   
+                     output += 
+                       `
+                   <div class="container col-4">
+                       <div class="row">
+                           <div class="card-wrapper">
+                           <div class="card">
+                           <div class="card-img-wrapper">
+                               <img class="card-img-top" src='${value.image_url}' alt="Card image cap">
+                       </div>
+                       <div class="card-body">
+                           <h5 class="card-title">${value.title}</h5>
+                           <div class="card-content">
+                               <p class="card-text">Amount of episodes:${value.episodes}</p>
+                               <p class="card-text">Score:${value.score}</p>
+                               <p class="card-text">Synopsis:${value.synopsis}</p>
+                               <a href="#" class="btn btn-primary" id="goToReviews">See Reviews</a>
+                           </div>
+                       </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+                   `;
+                 
+                
+                });
+                $('#animeResults').html(output);
+                $('#searchBar').val('');
+                
+            }   
 
-});
+        });
 
-
-
-
-$('#goToReviews').click(function(e){
-
-    $.ajax({
-        type:'GET',
-        url:'https://api.jikan.moe/v3/anime/269/reviews',
-        success:function(data){
-            console.log('success',data.reviews[0]);
-        }
-    
     });
 
-});
+    
+  
 
+              
+             
 
-
+         
+         
+         
+        
+           
+    
